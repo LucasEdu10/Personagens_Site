e1cea7a3e9828540b78f161d367235efacaf62bb
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2020-05-05 22:48:52 -0300

    Ajustes no projeto, exibindo tela de gerador

---
+++ public/javascripts/script.js
@@ -0,0 +1,26 @@
+function mostrar(){
+    var perso = window.document.getElementsByTagName('input')[0]
+    var img = document.createElement('img')
+    var res = window.document.querySelector('div#resultado')
+    img.setAttribute('id', 'imagem')
+
+    if(perso.value == 'Wilder'){
+        img.setAttribute('src','/images/perola.jpeg')
+        res.innerHTML = '<p><strong>Você é a Perola, pois é delicada e astuta como ela!!</strong></p>'
+    }else if(perso.value == 'George'){
+        img.setAttribute('src','/images/garnet.jpeg')
+        res.innerHTML = '<p><strong>Você é a Garnet, pois você é calmo e estóico!!</strong></p>'
+    }else if(perso.value == 'Luan'){
+        res.innerHTML = '<p><strong>Desculpe não foi possivel encontrar um personagem para você</strong></p>'
+    }else if(perso.value == 'Lucas'){
+        img.setAttribute('src','/images/steven.jpeg')
+        res.innerHTML = '<p><strong>Você é o Steven, pois você é otimista, amigável e extrovertido!!</strong></p>'
+    }else if(perso.value == 'Tayná'){
+        img.setAttribute('src', '/images/magali.png')
+        res.innerHTML = '<p><strong>Você é a magali, pois não para de comer!!</strong></p>'
+    }else{
+        res.innerHTML = '<p><strong>Digite o seu nome com a primeira letra Maiúscula!!</strong></p>'
+    }
+    perso.value = ''
+    res.appendChild(img)
+}///home/lucas/Projetos/Estudos/Personagens_Site/public/images/amentista.jpeg
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Personagem page. */
+router.get('/perso', function(req, res, next) {
+  res.render('modelo', { title: 'Personagem' });
+});
+
+
 module.exports = router;
